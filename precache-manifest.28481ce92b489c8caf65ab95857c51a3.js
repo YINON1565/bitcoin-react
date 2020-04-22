de8655baebf59acaa57718ff1c40912f4c4a25e0
Yinon_Eliyahu <yinoneliyaoo@gmail.com>
2020-04-22 15:25:00 +0300

    Updates

---
+++ precache-manifest.28481ce92b489c8caf65ab95857c51a3.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9bfcfe43e066656985ab39a4d22b892",
+    "url": "/bitcoin-react/index.html"
+  },
+  {
+    "revision": "ecd2421a0217f6343272",
+    "url": "/bitcoin-react/static/css/main.7f93b061.chunk.css"
+  },
+  {
+    "revision": "6f7cb8b2071ec8c20522",
+    "url": "/bitcoin-react/static/js/2.b30316ac.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/bitcoin-react/static/js/2.b30316ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ecd2421a0217f6343272",
+    "url": "/bitcoin-react/static/js/main.70b080be.chunk.js"
+  },
+  {
+    "revision": "dd78f1b0191473eecdf2",
+    "url": "/bitcoin-react/static/js/runtime-main.509ace64.js"
+  },
+  {
+    "revision": "31c2153c0530e32553b31a49b3d70736",
+    "url": "/bitcoin-react/static/media/ArchitectsDaughter-Regular.31c2153c.ttf"
+  },
+  {
+    "revision": "7c94be72174267b3aac61b9ab4df2af7",
+    "url": "/bitcoin-react/static/media/BalooBhai2-Regular.7c94be72.ttf"
+  },
+  {
+    "revision": "56a9d689963e9e055a252666cc88bf3a",
+    "url": "/bitcoin-react/static/media/CinzelDecorative-Regular.56a9d689.ttf"
+  },
+  {
+    "revision": "b87db7b018e6feda642e15062563d70f",
+    "url": "/bitcoin-react/static/media/add-friend.b87db7b0.svg"
+  },
+  {
+    "revision": "94e3636b72eb47b4abc19ddc6b9cb843",
+    "url": "/bitcoin-react/static/media/bin.94e3636b.svg"
+  },
+  {
+    "revision": "fe0aee18a6cd6c108cba789b8c339074",
+    "url": "/bitcoin-react/static/media/bitcoin-logo.fe0aee18.svg"
+  },
+  {
+    "revision": "8a47121e907f4bad1429998002df04a8",
+    "url": "/bitcoin-react/static/media/bitcoin.8a47121e.svg"
+  },
+  {
+    "revision": "d79c9bba7eed19735cc70cf011d436b8",
+    "url": "/bitcoin-react/static/media/go-back.d79c9bba.svg"
+  },
+  {
+    "revision": "b0b6390168d4fbc1b723fb54358dd78a",
+    "url": "/bitcoin-react/static/media/homepage.b0b63901.svg"
+  },
+  {
+    "revision": "4286fb8caff521bcfa724ddc6ab43c2c",
+    "url": "/bitcoin-react/static/media/line-chart.4286fb8c.svg"
+  },
+  {
+    "revision": "772a34ce90e9e681ba71b05a22151ee6",
+    "url": "/bitcoin-react/static/media/loading.772a34ce.svg"
+  },
+  {
+    "revision": "b9ed2e38505b6fe613fdf1c2ba81234f",
+    "url": "/bitcoin-react/static/media/log-in.b9ed2e38.svg"
+  },
+  {
+    "revision": "9b27cd33cd29d5bc9278601b0db31af6",
+    "url": "/bitcoin-react/static/media/pen.9b27cd33.svg"
+  },
+  {
+    "revision": "8f1410b1dbf25516a79ca68cb5f1afab",
+    "url": "/bitcoin-react/static/media/send.8f1410b1.svg"
+  },
+  {
+    "revision": "0efa1f2c0c1b5529a18e45cdca6280c4",
+    "url": "/bitcoin-react/static/media/user.0efa1f2c.svg"
+  }
+]);
